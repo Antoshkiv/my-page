517f03c3a5aa0d6197f95f92bd633ba64c2f4b8f
Antoshkiv <43931364+Antoshkiv@users.noreply.github.com>
2019-04-11 20:04:32 +0300

    fech add

---
+++ form.php
@@ -0,0 +1,10 @@
+    
+<p <p class="big-names">
+    Welcome <?php echo $_POST["name"]; ?><br>
+    We are waiting for you ! <br>
+    Details of your order: <br>
+    Date: <?php echo $_POST["trip-start"]; ?> <br>
+    Time: <?php echo $_POST["time"]; ?><br>
+    Person: <?php echo $_POST["person"]; ?> <br>
+    For more information call us +380-900-8003
+</p>
