068315d33582deee1eea396edb8b42535792b9ab
Dima <antoshkiv1@gmail.com>
2019-04-12 15:21:02 +0000

    gfinal ver

--- form.php
+++ form.php
@@ -4,7 +4,7 @@
     We are waiting for you ! <br>
     Details of your order: <br>
     Date: <?php echo $_POST["trip-start"]; ?> <br>
-    Time: <?php echo $_POST["time"]; ?><br>
+    Time: <?php echo $_POST["appt-time"]; ?><br>
     Person: <?php echo $_POST["person"]; ?> <br>
     For more information call us +380-900-8003
 </p>
